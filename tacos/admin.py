de256bbe1d26f9cdec2c14ca82723b8d41e23765
Pavel Kalinichenko <pavel.kalinichenko@gmail.com>
2016-05-04 19:51:01 +0300

    Initial model, some admin change, adding env variables for DB

--- tacos/admin.py
+++ tacos/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Recipe, Step, StepIngredient, Ingredient, UserProfile, Allergy
+from .forms import UserCreationForm
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Recipes description", {"fields": ["description"]}),
+        ("Recipes duration in minutes", {"fields": ["duration"]})
+    ]
+
+    # list_editable = ("duration",)
+
+    def recipe_directions(self, obj: Recipe):
+        return obj.list_directions()
+
+    recipe_directions.short_description = "Directions"
+
+    list_display = ("name", "description", "duration", "recipe_directions",)
+    list_display_links = ("name", "description", "duration",)
+    list_filter = ("duration",)
+    search_fields = ("name",)
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    # list_display = ("first_name", "last_name", "star",)
+    readonly_fields = ("star",)
+
+
+class MyUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2')}
+         ),
+    )
+
+
+@admin.register(Allergy)
+class AllergyAdmin(admin.ModelAdmin):
+    list_display = ("name", "severity", "get_user")
+
+    def get_user(self, obj: UserProfile):
+        return obj.get_user()
+
+    get_user.short_description = "User"
+
+
+admin.site.register(Step)
+admin.site.register(StepIngredient)
+admin.site.register(Ingredient)
+# admin.site.register(RecipeFavourite)
--- tacos/models.py
+++ tacos/models.py
@@ -1,3 +1,107 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tacos.core.models import TimeStampedModel
 
-# Create your models here.
+
+class Recipe(TimeStampedModel):
+    """
+        Recipes data model
+    """
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    duration = models.PositiveIntegerField()
+
+    def __str__(self):
+        return "Directions for {} are {}".format(self.name, self.list_directions())
+
+    def list_directions(self):
+        return ", ".join([step.directions for step in Step.objects.all().filter(recipe__id=self.id)])
+
+
+class Step(TimeStampedModel):
+    """
+        Steps data model
+    """
+    step_number = models.AutoField(primary_key=True)
+    recipe = models.ForeignKey(Recipe, related_name="%(class)s_recipe_id", on_delete=models.CASCADE)
+    directions = models.TextField()
+    duration = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.directions
+
+
+class Ingredient(TimeStampedModel):
+    """
+        Ingredients for recipes
+    """
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_name(self):
+        return self.name
+
+
+class StepIngredient(TimeStampedModel):
+    """
+        Amount of ingredients for each step in recipe
+    """
+    recipe = models.ForeignKey(Recipe, related_name="%(class)s_recipe_id", on_delete=models.CASCADE)
+    step_number = models.ForeignKey(Step, related_name="%(class)s_step_number", on_delete=models.CASCADE)
+    ingredient_id = models.ForeignKey(Ingredient, related_name="%(class)s_ingredient_id", on_delete=models.CASCADE)
+    quantity = models.CharField(max_length=255)
+
+    def __str__(self):
+        return ", ".join(
+            [ingredient.name for ingredient in Ingredient.objects.all().filter(stepingredient_ingredient_id=self.id)])
+
+
+class UserProfile(models.Model):
+    """
+        Additional data about User
+    """
+    user = models.OneToOneField(User)
+    star = models.BooleanField(default=False, verbose_name="Starred?")
+
+    # Override the __unicode__() method to return out something meaningful!
+    def __str__(self):
+        return self.user.username
+
+    def get_name(self):
+        return "{} {}".format(self.user.first_name, self.user.last_name)
+
+
+# class RecipeFavourite(TimeStampedModel):
+#     """
+#         Model for storing recipes favourites
+#     """
+#     # recipe = models.ForeignKey(Recipe, related_name="%(class)s_recipe_id", on_delete=models.CASCADE)
+#     # user = models.ForeignKey(UserProfile, related_name="%(class)s_user_id", on_delete=models.CASCADE)
+#     #
+#     # def list_favourites(self):
+#     #     return ", ".join([recipe.name for recipe in Recipe.objects.all().filter(recipe__id=self.id)])
+#     #
+#     # def __str__(self):
+#     #     return self.list_favourites()
+#     pass
+
+class Allergy(TimeStampedModel):
+    """
+        Model for storing Allergies with severity of condition
+    """
+    user = models.ForeignKey(UserProfile, related_name="%(class)s_userprofile_id")
+    # user = models.ForeignKey(UserProfile)
+    name = models.CharField(max_length=100)
+    LOW = "LOW"
+    MEDIUM = "MEDIUM"
+    HI = "HI"
+    severities = ((LOW, "LOW"), (MEDIUM, "MEDIUM"), (HI, "HI"))
+    severity = models.CharField(max_length=6, choices=severities, default=severities[1])
+
+    def __str__(self):
+        return "{} with {} severity".format(self.name, self.severity)
+
+    def get_user(self):
+        return self.user.get_name()
